a5f5afd9e270be24496fbaf30aebbec55bb6868b
Liam Collod <lcollod@gmail.com>
2023-10-15 11:04:26 +0200

    refactor(build): use new functions in rezbuild_utils

--- build.py
+++ build.py
@@ -1,10 +1,6 @@
 import logging
 import os
-import shutil
 import sys
-import tempfile
-import zipfile
-from pathlib import Path
 
 import rezbuild_utils
 
@@ -12,20 +8,7 @@ import rezbuild_utils
 LOGGER = logging.getLogger(__name__)
 
 
-def extract(downloaded_file: Path):
-    extract_root = downloaded_file.parent
-    LOGGER.info(f"extracting {downloaded_file.name} to {extract_root}...")
-    with zipfile.ZipFile(downloaded_file, "r") as zip_file:
-        zip_file.extractall(extract_root)
-    downloaded_file.unlink()
-
-    LOGGER.info(f"moving extracted file to proper location ...")
-    blender_exe = list(extract_root.glob("**/blender.exe"))[0]
-    for file in blender_exe.parent.glob("*"):
-        file.rename(extract_root / file.name)
-
-
-def _build(temp_folder: Path):
+def build():
     """
     We download blender in a local temp folder for faster speed.
     """
@@ -49,37 +32,13 @@ def _build(temp_folder: Path):
     blender_url += f"{blender_version_split[0]}.{blender_version_split[1]}"
     blender_url += f"/{blender_filename}"
 
-    blender_root_dir = temp_folder
-    blender_download_file_local = blender_root_dir / blender_filename
-    LOGGER.info(f"downloading {blender_url} ...")
-    rezbuild_utils.download_file(
+    rezbuild_utils.download_and_install_build(
         blender_url,
-        blender_download_file_local,
+        extract_reference_file_name="blender.exe",
         use_cache=True,
     )
 
-    target_dir = Path(os.environ["REZ_BUILD_INSTALL_PATH"])
-    LOGGER.info(f"copying {blender_download_file_local.name} to {target_dir} ...")
-    shutil.copy2(blender_download_file_local, target_dir)
-    blender_download_file_build = target_dir / blender_download_file_local.name
-
-    extract(blender_download_file_build)
-
     LOGGER.info("finished")
-
-
-def build():
-    prefix = f"{os.environ['REZ_BUILD_PROJECT_NAME']}-{os.environ['REZ_BUILD_PROJECT_VERSION']}-"
-    temp_folder = Path(tempfile.mkdtemp(prefix=prefix))
-    try:
-        _build(temp_folder)
-    except Exception as error:
-        # XXX: hack as progress bar doesn't add a new line if not finished
-        print("")
-        raise
-    finally:
-        LOGGER.info(f"removing temporary directory {temp_folder}")
-        shutil.rmtree(temp_folder)
 
 
 if __name__ == "__main__":
